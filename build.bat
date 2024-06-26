d51767a319ded1345d375889f6b2b576714ae645
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-26 14:06:47 +0500

    (chore): Put vcvars64.bat parent folder in PATH variable

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 mkdir build > nul 2>&1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" > nul 2>&1
+call vcvars64.bat > nul 2>&1
 pushd build
 cl -Zi ../handmade-hero.cpp user32.lib gdi32.lib
 popd
