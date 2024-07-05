44faf8e7e8c35884d240316dc0b939676ddb5fa1
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-05 23:19:54 +0500

    (chore): Update compile target file

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@
 mkdir build > nul 2>&1
 call vcvars64.bat > nul 2>&1
 pushd build
-cl -FC -Zi ../handmade-hero.cpp user32.lib gdi32.lib xinput.lib
+cl -FC -Zi ../win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib
 popd
