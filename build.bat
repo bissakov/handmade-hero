224dfa3069446b03ed07a0a7473f814df60aab8a
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-07 23:38:57 +0500

    (chore): Update compile target and link the library

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@
 mkdir build > nul 2>&1
 call vcvars64.bat > nul 2>&1
 pushd build
-cl -FC -Zi ../win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib
+cl -FC -Zi ../src/win32/win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib ../src/handmade-hero/handmade-hero.cpp
 popd
