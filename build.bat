8c0d4d46657660ed12675546bef0dc61109330d1
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-09 15:31:49 +0500

    (chore): Ignore warnings C4201 and C4127

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@
 mkdir build > nul 2>&1
 call vcvars64.bat > nul 2>&1
 pushd build
-cl -W4 -FC -Zi ../src/win32/win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib ../src/handmade-hero/handmade-hero.cpp
+cl -W4 -FC -Zi -wd4201 -wd4127 ../src/win32/win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib ../src/handmade-hero/handmade-hero.cpp
 popd
