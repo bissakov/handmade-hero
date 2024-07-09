cad681838f0645255cf740b8130fed3c17bcc234
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-09 16:02:31 +0500

    (chore): Allow building to Windows x86

--- build.bat
+++ build.bat
@@ -1,7 +1,19 @@
 @echo off
 
 mkdir build > nul 2>&1
-call vcvars64.bat > nul 2>&1
+
+set ARCH=64
+
+if "%1"=="32" (
+    set ARCH=32
+)
+
+if "%ARCH%"=="32" (
+    call vcvars32.bat > nul 2>&1
+) else (
+    call vcvars64.bat > nul 2>&1
+)
+
 pushd build
 cl -nologo -Oi -GR- -EHa- -W4 -FC -Z7 -wd4201 -wd4127 ../src/win32/win32-handmade-hero.cpp user32.lib gdi32.lib xinput.lib ../src/handmade-hero/handmade-hero.cpp
 popd
